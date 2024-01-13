8281b93a39c34b50be29cbe6eff35cac80824b09
Taxze <1929509811@qq.com>
2024-01-13 09:10:53 +0800

    barrage

--- lib/pages/mine/mine_story_details/widget/barrage/barrage_view.dart
+++ lib/pages/mine/mine_story_details/widget/barrage/barrage_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class BarrageView extends StatefulWidget {
   const BarrageView({Key? key}) : super(key: key);
@@ -12,9 +13,30 @@ class _BarrageViewState extends State<BarrageView> {
   @override
   Widget build(BuildContext context) {
     return Stack(
-      children: [
-
-      ],
+      children: [],
     );
+  }
+
+  static Size getDanmakuBulletSizeByText(String text) {
+    const constraints = BoxConstraints(
+      maxWidth: 999.0, // maxwidth calculated
+      minHeight: 0.0,
+      minWidth: 0.0,
+    );
+    RenderParagraph renderParagraph = RenderParagraph(
+      TextSpan(
+        text: text,
+        style: const TextStyle(
+          fontSize: 14,
+        ),
+      ),
+      textAlign: TextAlign.center,
+      textDirection: TextDirection.ltr,
+      maxLines: 1,
+    );
+    renderParagraph.layout(constraints);
+    double w = renderParagraph.getMinIntrinsicWidth(14).ceilToDouble();
+    double h = renderParagraph.getMinIntrinsicHeight(999).ceilToDouble();
+    return Size(w, h);
   }
 }
