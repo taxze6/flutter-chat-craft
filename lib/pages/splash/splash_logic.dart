9c100457356a48cdea8781920270287f597c3735
Min <adrien6727@gmail.com>
2024-01-28 00:25:13 +0800

    🏝️Remember token

--- lib/pages/splash/splash_logic.dart
+++ lib/pages/splash/splash_logic.dart
@@ -1,6 +1,19 @@
+import 'package:flutter_chat_craft/common/global_data.dart';
+import 'package:flutter_chat_craft/models/user_info.dart';
 import 'package:flutter_chat_craft/routes/app_navigator.dart';
+import 'package:flutter_chat_craft/utils/sp/data_persistence.dart';
 import 'package:get/get.dart';
 
-class SplashLogic extends GetxController{
-  void enterChatCraft() => AppNavigator.startLogin();
+class SplashLogic extends GetxController {
+  void enterChatCraft() {
+    String token = DataPersistence.getToken();
+    if (token == '') {
+      AppNavigator.startLogin();
+    } else {
+      UserInfo user = DataPersistence.getUserInfo();
+      GlobalData.userInfo = user;
+      GlobalData.token = token;
+      AppNavigator.startHome();
+    }
+  }
 }
