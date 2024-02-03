69edd8ad53aa9ee79a35e2e2cee9b18a282235c5
Taxze <1929509811@qq.com>
2024-02-03 22:33:38 +0800

    🎨Handle message state drawing details

--- lib/pages/chat/chat/widget/chat_typing_view.dart
+++ lib/pages/chat/chat/widget/chat_typing_view.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_craft/res/styles.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ChatTypingWidget extends StatefulWidget {
@@ -15,28 +16,34 @@ class _ChatTypingWidgetState extends State<ChatTypingWidget>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _repaintAnimation;
+  List<double> xAliax1 = [];
+  List<double> xAliax2 = [];
+  List<double> xAliax3 = [];
 
   @override
   void initState() {
     super.initState();
+    initPoints();
     _controller = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 1999),
     );
     _repaintAnimation = Tween<double>(
       begin: 0.0,
-      end: 1.0,
+      end: 0.25,
     ).animate(CurvedAnimation(
       parent: _controller,
       curve: Curves.linear,
     ));
     _controller.forward();
   }
+
   @override
   void dispose() {
     super.dispose();
     _controller.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -48,48 +55,50 @@ class _ChatTypingWidgetState extends State<ChatTypingWidget>
         borderRadius: BorderRadius.circular(12.0),
       ),
       child: Center(
-        child: AnimatedBuilder(
-          animation: _repaintAnimation,
-          builder: (context, child) {
-            return CustomPaint(
-              painter: TypingPainter(_repaintAnimation),
-              size: Size(30.w, 20.w),
-            );
-          },
-        ),
-      ),
+          child: Stack(
+        children: [
+          AnimatedBuilder(
+            animation: _repaintAnimation,
+            builder: (context, child) {
+              return CustomPaint(
+                painter: TypingPainter(
+                  repaint: _repaintAnimation,
+                  color: Colors.black,
+                  xAliax: xAliax1,
+                ),
+                size: Size(30.w, 20.w),
+              );
+            },
+          ),
+          AnimatedBuilder(
+            animation: _repaintAnimation,
+            builder: (context, child) {
+              return CustomPaint(
+                painter: TypingPainter(
+                  repaint: _repaintAnimation,
+                  color: Colors.red,
+                  xAliax: xAliax2,
+                ),
+                size: Size(30.w, 20.w),
+              );
+            },
+          ),
+          AnimatedBuilder(
+            animation: _repaintAnimation,
+            builder: (context, child) {
+              return CustomPaint(
+                painter: TypingPainter(
+                  repaint: _repaintAnimation,
+                  color: PageStyle.chatColor,
+                  xAliax: xAliax3,
+                ),
+                size: Size(30.w, 20.w),
+              );
+            },
+          ),
+        ],
+      )),
     );
-  }
-}
-
-class TypingPainter extends CustomPainter {
-  final Animation<double> repaint;
-  List xAliax = [];
-
-  TypingPainter(this.repaint);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    // 获取采样点
-    initPoints();
-    // 画笔
-    Paint paint = Paint()
-      ..color = Colors.red
-      ..strokeWidth = 2
-      ..style = PaintingStyle.stroke;
-    // 路径
-    Path path = Path();
-    canvas.translate(-25.w, size.height / 2);
-    // 通过点确定曲线路径
-    for (var i = 1; i < xAliax.length - 1; i++) {
-      double x1 = xAliax[i];
-      double y1 = funcSquaredSinx(x1);
-      double x2 = (xAliax[i] + xAliax[i + 1]) / 2;
-      double y2 = (y1 + funcSquaredSinx(xAliax[i + 1])) / 2;
-      path.quadraticBezierTo(x1, y1, x2, y2);
-    }
-    // 画布绘制
-    canvas.drawPath(path, paint);
   }
 
   void initPoints() {
@@ -100,14 +109,54 @@ class TypingPainter extends CustomPainter {
 
     // 生成采样点
     for (double x = start; x <= end; x += step) {
-      xAliax.add(x);
+      xAliax1.add(x);
     }
+  }
+}
+
+class TypingPainter extends CustomPainter {
+  final Animation<double> repaint;
+  final Color color;
+  final List<double> xAliax;
+
+  TypingPainter({
+    required this.repaint,
+    required this.xAliax,
+    this.color = Colors.black,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // 画笔
+    Paint paint = Paint()
+      ..color = color
+      ..strokeWidth = 2
+      ..style = PaintingStyle.stroke;
+    // 路径
+    Path path = Path();
+    canvas.translate(-25.w, size.height / 2);
+    // 通过点确定曲线路径
+    for (var i = 1; i < xAliax.length - 1; i++) {
+      double x1 = xAliax[i];
+      double y1 = funcSquaredSinX(x1);
+      double x2 = (xAliax[i] + xAliax[i + 1]) / 2;
+      double y2 = (y1 + funcSquaredSinX(xAliax[i + 1])) / 2;
+      path.quadraticBezierTo(x1, y1, x2, y2);
+    }
+    // 画布绘制
+    canvas.drawPath(path, paint);
   }
 
   //曲线函数表达式
-  double funcSquaredSinx(double x) {
+  double funcSquaredSinX(double x) {
     double p =
         10 * sin(3 * pi * x / 200 - 100 * repaint.value) * sin(x * pi / 100);
+    return p;
+  }
+
+  double funcSquaredCosX(double x) {
+    double p =
+        10 * cos(3 * pi * x / 200 - 100 * repaint.value) * cos(x * pi / 100);
     return p;
   }
 
