50c592b0f592e9313d9a4ee4bb1866d8d138e76f
徐臻杰 <1929509811@qq.com>
2024-03-15 16:08:24 +0800

    🎃Add file size tests and hash tests.

--- lib/pages/splash/splash_logic.dart
+++ lib/pages/splash/splash_logic.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:crypto/crypto.dart';
 import 'package:flutter_chat_craft/common/global_data.dart';
 import 'package:flutter_chat_craft/models/user_info.dart';
 import 'package:flutter_chat_craft/routes/app_navigator.dart';
@@ -17,8 +20,47 @@ class SplashLogic extends GetxController {
     }
   }
 
-  Future<bool> checkLocalFile() async {
+  Future<bool> checkLocalFile(String filePath) async {
     //Check whether the App resource is complete.
-    return false;
+    if (!File(filePath).existsSync()) {
+      print('File does not exist');
+      return false;
+    }
+
+    // Get file size
+    File file = File(filePath);
+    int fileSize = await file.length();
+
+    print('File integrity');
+    return true;
+  }
+
+  ///Compare file sizes
+  Future<bool> isFileCompleteBySize(String filePath, int expectedSize) async {
+    File file = File(filePath);
+    int fileSize = await file.length();
+    if (fileSize == expectedSize) {
+      print('File integrity');
+      return true;
+    } else {
+      print('Incomplete file');
+      return false;
+    }
+  }
+
+  Future<bool> isFileCompleteByHash(String filePath, String expectedHash) async {
+    File file = File(filePath);
+    List<int> fileBytes = await file.readAsBytes();
+
+    // Calculates the hash value of the file
+    String fileHash = md5.convert(fileBytes).toString();
+
+    if (fileHash == expectedHash) {
+      print('File integrity');
+      return true;
+    } else {
+      print('Incomplete file');
+      return false;
+    }
   }
 }
