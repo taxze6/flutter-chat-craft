2efc89530ac17e7a6087ebbc5aa418d8bdeb0948
徐臻杰 <1929509811@qq.com>
2024-04-14 20:59:37 +0800

    😳Add some call video enum.

--- lib/pages/chat/rtc/video_call/video_call_logic.dart
+++ lib/pages/chat/rtc/video_call/video_call_logic.dart
@@ -4,6 +4,25 @@ enum VideoAction {
   cancel,
   connect,
 }
+enum SignalingState {
+  connectionOpen,
+  connectionClosed,
+  connectionError,
+}
+
+enum CallState {
+  callStateNew,
+  callStateRinging,
+  callStateInvite,
+  callStateConnected,
+  callStateBye,
+}
+
+enum VideoSource {
+  camera,
+  screen,
+}
+
 
 class VideoCallLogic extends GetxController {
 
